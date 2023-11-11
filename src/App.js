173a7e97a2dd5142bc05a50efd3204e28de852c0
Alex Keener <alexkeener318@gmail.com>
2023-11-10 20:52:56 -0600

    Cleans up App.js

--- src/App.js
+++ src/App.js
@@ -5,38 +5,7 @@ import { Link } from 'react-router-dom';
 import Sliders from './components/sliders';
 
 function App() {
-  const [happyValue, setHappyValue] = useState(50);
-  const [sadValue, setSadValue] = useState(50);
-  const [energyValue, setEnergyValue] = useState(50);
-  const [calmnessValue, setCalmnessValue] = useState(50);
-  const [danceabilityValue, setDanceabilityValue] = useState(50);
-
-  const handleHappyValue = (event, newValue) => {
-    setHappyValue(newValue);
-  };
-
-  const handleSadValue = (event, newValue) => {
-    setSadValue(newValue);
-  };
-
-  const handleEnergyValue = (event, newValue) => {
-    setEnergyValue(newValue);
-  };
-
-  const handleCalmnessValue = (event, newValue) => {
-    setCalmnessValue(newValue);
-  };
   
-  const handleDanceabilityValue = (event, newValue) => {
-    setDanceabilityValue(newValue);
-  };  
-
-  const handleSubmit = () => {
-    const message = `Happy Value: ${happyValue}, Sad Value: ${sadValue}, Energetic Value: ${energyValue}, Calmness Value: ${calmnessValue}, Danceability Value: ${danceabilityValue}`;
-    alert(message);
-  };
-  
-
   return (
     <div className="App">
       
