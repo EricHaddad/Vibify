90325bdb78c466062c931e2abdd637bd8f1679ec
Alex Keener <alexkeener318@gmail.com>
2023-11-28 22:22:02 -0600

    Adds more functionality to song details page

--- src/Routes.js
+++ src/Routes.js
@@ -9,12 +9,13 @@ const AppRoutes = () => {
   const [song, setSong] = useState(null);
   const [accessToken, setAccessToken] = useState(null);
   const [moodState, setMoodState] = useState("");
+  const [songList, setSongList] = useState(null);
 
   return (
     <Routes>
       <Route path="/addSong" element={<AddSong />} />
-      <Route path="/" element={<App setSong={setSong} setMood={setMoodState} setToken={setAccessToken}/>} />
-      <Route path="/songDetails" element={<SongDetails song={song} mood={moodState} accessToken={accessToken}/>} />
+      <Route path="/" element={<App setSong={setSong} setMood={setMoodState} setToken={setAccessToken} setSongList={setSongList}/>} />
+      <Route path="/songDetails" element={<SongDetails song={song} mood={moodState} accessToken={accessToken} songList={songList} setSong={setSong}/>} />
     </Routes>
   );
 };
